627c8b96a455cb64ea4231f8c61d8432f916a0d8
anureddyyeruva <anureddyyeruva6@gmail.com>
2018-06-04 16:14:55 +0530

    Second Commit

--- app/src/main/java/com/grepthor/krishnaapp/MainActivity.java
+++ app/src/main/java/com/grepthor/krishnaapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//write something here
+//write something here vud
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
